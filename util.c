krishnatayal/strace

Add PAF_ARRAY_TRUNCATED flag for print_array_ex
PAF_ARRAY_TRUNCATED allows enforcing the fact that an array is truncated
which is useful for arrays in local memory that are known as being
truncated.

* defs.h (xlat_style_private_flag_bits): Add PAF_ARRAY_TRUNCATED_BIT.
(xlat_style_private_flags): Add PAF_ARRAY_TRUNCATED.
* util.c (print_array_ex): Handle PAF_ARRAY_TRUNCATED in flags.

util.c
@@ -1282,8 +1282,17 @@ print_array_ex(struct tcb *const tcp,
 			break;
 		}
 	}
-	if ((cur != start_addr) || !tfetch_mem_func)
+
+	if ((cur != start_addr) || !tfetch_mem_func) {
+		if (flags & PAF_ARRAY_TRUNCATED) {
+			if (cur != start_addr)
+				tprints(", ");
+
+			tprints("...");
+		}
+
 		tprints("]");
+	}
 
 	return cur >= end_addr;
 }

superbothq/superstrace

util.c
@@ -1360,8 +1360,17 @@ print_array_ex(struct tcb *const tcp,
 			break;
 		}
 	}
-	if ((cur != start_addr_l) || !tfetch_mem_func)
+
+	if ((cur != start_addr_l) || !tfetch_mem_func) {
+		if (flags & PAF_ARRAY_TRUNCATED) {
+			if (cur != start_addr_l)
+				tprints(", ");
+
+			tprints("...");
+		}
+
 		tprints("]");
+	}
 
 	return cur >= end_addr;
 }